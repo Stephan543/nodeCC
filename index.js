61a627f6eb8f087ccdead51a55553a72a87d091a
Stephan Iskander <stephan.iskander@gmail.com>
2021-02-18 09:37:27 -0500

    Heroku deploy

--- index.js
+++ index.js
@@ -1,7 +1,84 @@
-console.log('Hello From Node.js...');
+// Core modules - can be simplified w/ express
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const Person = require('./person'); //common JS
+const server = http.createServer((req, res) => {
+  //   if (req.url === "/") {
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "index.html"),
+  //       (err, content) => {
+  //           if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" }); //Status 200 means OK
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+  // // Hardcoding potential microservice request, API example
+  //   if (req.url === "/api/users") {
+  //    const users = [
+  //        { name: 'Stephan Iskander', age: 24},
+  //        { name: 'Brenda Eldebs', age: 25}
+  //    ];
+  //    res.writeHead(200, { 'Content-Type': 'application/json' })
+  //    res.end(JSON.stringify(users));
+  //   }
 
-const person1 = new Person('Stephan Isky', 24);
+  // Build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
 
-person1.greeting();
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content type
+  let contentType = 'text/html';
+
+  // Check ext and set content type
+  switch(extname) {
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.css':
+        contentType = 'text/css';
+        break;
+    case '.json':
+        contentType = 'application/json';
+        break;
+    case '.png':
+        contentType = 'image/png';
+        break;
+    case '.jpg':
+        contentType = 'image/jpg';
+        break;
+  }
+
+  // Read File
+  fs.readFile(filePath, (err, content) => {
+    if(err){
+        if(err.code == 'ENOENT'){
+            // Page not found
+            fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end(content, 'utf8');
+            })
+        } else {
+            // Some server error
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`)
+        }
+    } else {
+        // Success
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(content, 'utf8');
+    }
+  });
+});
+
+// Setting port, depending on where  I deploy, port number is given in the environment
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
